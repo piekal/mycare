3c13c4a13c4d152ab80dc834f85f3d9059b40c9b
oaks-view <captainoak@gmail.com>
2018-01-16 22:56:08 +0100

    completed password signup page

--- App/Containers/PasswordSigninScreen.js
+++ App/Containers/PasswordSigninScreen.js
@@ -35,7 +35,7 @@ class PasswordSigninScreen extends React.PureComponent {
                         color: '#F25050', fontSize: 11, lineHeight: 14,
                         textAlign: 'center'
                     }}>
-                        Password doesnt match
+                        Password should be at least 6 characters
                     </Text>
                 </Item>
             );
@@ -47,12 +47,12 @@ class PasswordSigninScreen extends React.PureComponent {
             <View style={{ flex: 6, justifyContent: 'space-between', paddingVertical: 70 }}>
                 <View style={[{ alignItems: 'center' }]}>
                     <Thumbnail large source={Images.ignite} />
-                    <Text uppercase={true} style={{fontSize: 10.5, lineHeight: 16}}>{this.state.user.fullName}</Text>
+                    <Text uppercase={true} style={{fontSize: 10.5, lineHeight: 16, fontWeight: '700'}}>{this.state.user.fullName}</Text>
                 </View>
                 <Item style={[SigninScreenStyles.inputField, this.passwordIsValid && SigninScreenStyles.inputValid, this.passwordIsInvalid && SigninScreenStyles.inputInvalid]} stackedLabel>
                     <Label style={[SigninScreenStyles.floatingLabel, this.passwordIsValid && SigninScreenStyles.labelValid,
                     this.passwordIsInvalid && SigninScreenStyles.labelInvalid, { alignSelf: 'center' }]}>Password</Label>
-                    <Input placeholderTextColor="#A8A8A8" style={[SigninScreenStyles.textInput]}
+                    <Input placeholderTextColor="#A8A8A8" style={[SigninScreenStyles.textInput]} secureTextEntry={true}
                         placeholder="******" onChangeText={(text) => this.setState({ password: text, passwordDirty: true })} value={this.state.password} />
                 </Item>
                 {this.showpasswordErrorMsg()}
@@ -85,7 +85,10 @@ class PasswordSigninScreen extends React.PureComponent {
                 }}>
                     <View style={{ flex: 1.5 }}>
                         {/* <Left><Icon name="arrow-back" /></Left> */}
-                        <Text style={{ color: "blue", fontSize: 32, fontWeight: 'bold', textAlign: 'center' }}>myCare</Text>
+                        <Text style={{ color: Colors.appBlue, fontSize: 24, lineHeight: 32, textAlign: 'center' }}>
+                            my<Text style={{ fontWeight: 'bold', color: Colors.appBlue, fontSize: 24, lineHeight: 32 }}>Care.</Text>
+                        </Text>
+                        {/* <Text style={{ color: "blue", fontSize: 32, fontWeight: 'bold', textAlign: 'center' }}>myCare</Text> */}
                     </View>
                     {this.renderpasswordForm()}
 
@@ -96,7 +99,7 @@ class PasswordSigninScreen extends React.PureComponent {
                         
                         <View style={{ flex: 0.5, marginTop: 15, borderBottomColor: '#FBFBFB', alignItems: 'center', justifyContent: 'center' }}>
                             <Text>Dont have an Account?
-                            <Text onPress={() => { this.props.navigation.navigate('Signup') }} style={{ color: '#1C58B5', fontSize: 14, lineHeight: 20 }}> SIGN-UP</Text></Text>
+                            <Text onPress={() => { this.props.navigation.navigate('Signup') }} style={{ color: '#1C58B5', fontSize: 14, lineHeight: 20, fontWeight: '700' }}> SIGN-UP</Text></Text>
                         </View>
                     </View>
                 </Content>
