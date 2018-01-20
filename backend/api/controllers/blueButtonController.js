0abc0ace474b1b8d26f93b31bd892a4ac097fc52
Peter Kim <peter.kim@newwave.io>
2018-01-20 16:32:38 -0500

    Parsing EOB

--- backend/api/controllers/blueButtonController.js
+++ backend/api/controllers/blueButtonController.js
@@ -9,7 +9,7 @@ var entry = mongoose.model('Entry');
 var billable = mongoose.model('Billable');
 var diagnosis = mongoose.model('Diagnosis');
 var ICDnum = mongoose.model('ICD');
-
+var _ = require('lodash');
 /*
  * handle provider callback
  */
@@ -25,7 +25,7 @@ exports.provider_callback = function(req, res) {
 exports.provider_callback = function(req, res) {
   console.log('GET /bb/provider/callback : ',req.params);
 
-/*   axios({
+  axios({
     method:'get',
     url:'https://sandbox.bluebutton.cms.gov/v1/fhir/ExplanationOfBenefit/?patient=20140000008324',
     headers: {
@@ -33,68 +33,66 @@ exports.provider_callback = function(req, res) {
     },
     responseType:'json'
   }).then(function(response) {
-    console.log(response.data);
-    return res.status(200).send();
-  }); */
-  var eobparse= JSON.parse(fs.readFileSync('../shared/SampleEOB.json'));
-  
+    //  var eobparse= JSON.parse(fs.readFileSync('../shared/SampleEOB.json'));
+    var eobparse = response.data;
+    parseEOB(req,eobparse);    
+  });
+  return res.status(200).send();
+}
+
+function parseEOB(req,eobparse) {
+
+  console.log("Start Parse EOB");
+
   var eob = new EOB({ user_id:req.user, total: eobparse.total});
   
- 	
-	
-  
-  // save eob
-  eob.save(function(err, eob) {
-	 var entryl = new entry({ url: eobparse.entry[0].fullUrl}); 
-	  entryl.eob_id = eob;
-	    
-		//save entry
-		entryl.save(function(err, entryl){
-			var bill = new billable({
-				startDate:eobparse.entry[0].resource.billablePeriod.start, 
-				endDate:eobparse.entry[0].resource.billablePeriod.end
-				});
-			bill.entry_id = entryl;
-			bill.save();
-			
-			var diag1 = new diagnosis({
-				sequence:eobparse.entry[0].resource.diagnosis[0].sequence,
-				icd_version:eobparse.entry[0].resource.diagnosis[0].diagnosisCodeableConcept.coding[0].system
-			});
-			diag1.entry_id = entryl;
-			
-			ICDnum.count().exec(function (err, count) {
-				console.error("ICD count : ",count);
-				var random = Math.floor(Math.random() * count);
-			    ICDnum.findOne().skip(random).exec(
-					function (err, result) {
-						console.error(result);
-						diag1.icd_code = result.code;
-						diag1.save();
-				});	
-			});
-		});
-  });
-
-  //console.error(eobparse.entry[0].resource);
-    // find eob type
+  // find eob type
   EOBType.findOne({
-	  name: "CMS_BLUE_BUTTON"
+    name: "CMS_BLUE_BUTTON"
   }).then(function(res) {
-	  console.error(res);
-	  eob.eob_type = res;
-	  eob.save();
-  });
-  
-  
-  
-  
-  console.error(req.user);
-  
-  
-  //newType.save();
-  //bill.save();
-  return res.status(200).send();
+    console.log(res);
+    eob.eob_type = res;
+    // save eob
+    eob.save(function(err, eob) {
+
+      // loop entries
+      _.each(eobparse.entry, function(e) {
+        console.log("EOB url : ",e.fullUrl);
+        var entryl = new entry({ url: e.fullUrl}); 
+        entryl.eob_id = eob;
+        
+        //save entry
+        entryl.save(function(err, entryl) {
+          var bill = new billable({
+	    startDate:e.resource.billablePeriod && e.resource.billablePeriod.start, 
+	    endDate:e.resource.billablePeriod && e.resource.billablePeriod.end
+          });
+          bill.entry_id = entryl;
+          bill.save();
+
+          // loop diagonosis
+          _.each(e.resource.diagnosis, function(d) {
+            var diag1 = new diagnosis({
+	      sequence:d.sequence,
+	      icd_version:d.diagnosisCodeableConcept.coding[0].system
+            });
+            diag1.entry_id = entryl;
+            
+            ICDnum.count().exec(function (err, count) {
+	      console.log("ICD count : ",count);
+	      var random = Math.floor(Math.random() * count);
+	      ICDnum.findOne().skip(random).exec(
+	        function (err, result) {
+	          console.log(result);
+	          diag1.icd_code = result.code;
+	          diag1.save();
+	        });	
+            });
+          })
+        });
+      });
+    });
+  });  
 }
 
 /*
